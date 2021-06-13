ccca4a9a3f64935ccf7f8866a837cf7fa0c18795
uujnas <sanjubasnet036@gmail.com>
2021-06-13 17:37:15 +0545

    developed using javascript

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,127 +1,100 @@
-// HTML Elements
-const statusDiv = document.querySelector('.status');
-const resetDiv = document.querySelector('.reset');
-const cellDivs = document.querySelectorAll('.game-cell');
+// element = document.querySelector(Selector); return element within the document 
 
-// game constants
-const xSymbol = '×';
-const oSymbol = '○';
-
-// game variables
-let gameIsLive = true;
-let xIsNext = true;
+const box = document.querySelectorAll(".box"); //querySelectorAll:all elements matching the selector
+const gameBox = document.querySelector(".game-box");
+const x = 'cross'; //variable decleratation
+const y = 'circle';
+const target = document.getElementById('txt');
+let temp;
 
 
-// functions
-const letterToSymbol = (letter) => letter === 'x' ? xSymbol : oSymbol;
+const winCondition = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6]
+]
 
-const handleWin = (letter) => {
-  gameIsLive = false;
-  if (letter === 'x') {
-    statusDiv.innerHTML = `${letterToSymbol(letter)} has won!`;
-  } else {
-    statusDiv.innerHTML = `<span>${letterToSymbol(letter)} has won!</span>`;
-  }
-};
-
-const checkGameStatus = () => {
-  const topLeft = cellDivs[0].classList[1];
-  const topMiddle = cellDivs[1].classList[1];
-  const topRight = cellDivs[2].classList[1];
-  const middleLeft = cellDivs[3].classList[1];
-  const middleMiddle = cellDivs[4].classList[1];
-  const middleRight = cellDivs[5].classList[1];
-  const bottomLeft = cellDivs[6].classList[1];
-  const bottomMiddle = cellDivs[7].classList[1];
-  const bottomRight = cellDivs[8].classList[1];
-
-  // check winner
-  if (topLeft && topLeft === topMiddle && topLeft === topRight) {
-    handleWin(topLeft);
-    cellDivs[0].classList.add('won');
-    cellDivs[1].classList.add('won');
-    cellDivs[2].classList.add('won');
-  } else if (middleLeft && middleLeft === middleMiddle && middleLeft === middleRight) {
-    handleWin(middleLeft);
-    cellDivs[3].classList.add('won');
-    cellDivs[4].classList.add('won');
-    cellDivs[5].classList.add('won');
-  } else if (bottomLeft && bottomLeft === bottomMiddle && bottomLeft === bottomRight) {
-    handleWin(bottomLeft);
-    cellDivs[6].classList.add('won');
-    cellDivs[7].classList.add('won');
-    cellDivs[8].classList.add('won');
-  } else if (topLeft && topLeft === middleLeft && topLeft === bottomLeft) {
-    handleWin(topLeft);
-    cellDivs[0].classList.add('won');
-    cellDivs[3].classList.add('won');
-    cellDivs[6].classList.add('won');
-  } else if (topMiddle && topMiddle === middleMiddle && topMiddle === bottomMiddle) {
-    handleWin(topMiddle);
-    cellDivs[1].classList.add('won');
-    cellDivs[4].classList.add('won');
-    cellDivs[7].classList.add('won');
-  } else if (topRight && topRight === middleRight && topRight === bottomRight) {
-    handleWin(topRight);
-    cellDivs[2].classList.add('won');
-    cellDivs[5].classList.add('won');
-    cellDivs[8].classList.add('won');
-  } else if (topLeft && topLeft === middleMiddle && topLeft === bottomRight) {
-    handleWin(topLeft);
-    cellDivs[0].classList.add('won');
-    cellDivs[4].classList.add('won');
-    cellDivs[8].classList.add('won');
-  } else if (topRight && topRight === middleMiddle && topRight === bottomLeft) {
-    handleWin(topRight);
-    cellDivs[2].classList.add('won');
-    cellDivs[4].classList.add('won');
-    cellDivs[6].classList.add('won');
-  } else if (topLeft && topMiddle && topRight && middleLeft && middleMiddle && middleRight && bottomLeft && bottomMiddle && bottomRight) {
-    gameIsLive = false;
-    statusDiv.innerHTML = 'Game is tied!';
-  } else {
-    xIsNext = !xIsNext;
-    if (xIsNext) {
-      statusDiv.innerHTML = `${xSymbol} is next`;
-    } else {
-      statusDiv.innerHTML = `<span>${oSymbol} is next</span>`;
-    }
-  }
-};
-
-
-// event Handlers
-const handleReset = () => {
-  xIsNext = true;
-  statusDiv.innerHTML = `${xSymbol} is next`;
-  for (const cellDiv of cellDivs) {
-    cellDiv.classList.remove('x');
-    cellDiv.classList.remove('o');
-    cellDiv.classList.remove('won');
-  }
-  gameIsLive = true;
-};
-
-const handleCellClick = (e) => {
-  const classList = e.target.classList;
-
-  if (!gameIsLive || classList[1] === 'x' || classList[1] === 'o') {
-    return;
-  }
-
-  if (xIsNext) {
-    classList.add('x');
-    checkGameStatus();
-  } else {
-    classList.add('o');
-    checkGameStatus();
-  }
-};
-
-
-// event listeners
-resetDiv.addEventListener('click', handleReset);
-
-for (const cellDiv of cellDivs) {
-  cellDiv.addEventListener('click', handleCellClick)
+function placemarker(currentClass, currentCell) {
+	currentCell.classList.add(currentClass);
 }
+
+function swapturns() {
+	temp = !temp; //reverse change position
+}
+
+function showHoverEffect(currentClass) {
+	gameBox.classList.remove(x);
+	gameBox.classList.remove(y);
+	if (temp) {
+		gameBox.classList.add(y)
+	} else {
+		gameBox.classList.add(x)
+	}
+
+
+}
+
+//check winner
+function checkwin(currentClass){
+	return winCondition.some(item => {
+		return item.every(index =>{
+			return box[index].classList.contains(currentClass);
+		})
+	})
+}
+
+function mainfunc(e) {
+	console.log("clicked");
+
+	currentClass = temp ? 'circle' : 'cross';
+	placemarker(currentClass, e.target);
+	if(checkwin(currentClass)){
+		endgame(true);
+	}else if(drawGame()){
+		endgame(false);
+		console.log("draw")
+
+	}
+	
+	swapturns()
+	showHoverEffect(currentClass);
+}
+function endgame(value){
+	let txt;
+	if(value){
+		txt = temp?'0 win!' :'x win!'
+
+	}else{
+		txt='draw';
+
+	}
+	target.innerText=txt;
+}
+
+//check draw
+function drawGame(){
+	return [...box].every(cell =>{
+		return cell.classList.contains(x) || cell.classList.contains(y);
+	})
+}
+
+function startGame() {
+	temp = false;
+	showHoverEffect();
+	//main function
+	box.forEach(cell => {
+		cell.addEventListener('click', mainfunc, {
+			once: true
+		});
+		//element.addEventListener(event,function,useCapture)
+		//forEach : call element only once
+
+	})
+}
+
+startGame();
